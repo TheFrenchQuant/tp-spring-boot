a71ce47fdccc6bc743e3898cea909f3e6147a095
Franck Salzard <franck.salzard@etu.univ-grenoble-alpes.f>
2022-11-16 19:21:35 +0100

    jpa

--- jpa/src/main/java/com/example/jpa/ProductRepository.java
+++ jpa/src/main/java/com/example/jpa/ProductRepository.java
@@ -1,4 +1,4 @@
-package com.example.product;
+package com.example.jpa;
 
 //import java.util.List;
 
--- jpa/src/test/java/com/example/jpa/JpaApplicationTests.java
+++ jpa/src/test/java/com/example/jpa/JpaApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.product;
+package com.example.jpa;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProductApplicationTests {
+class JpaApplicationTests {
 
 	@Test
 	void contextLoads() {
