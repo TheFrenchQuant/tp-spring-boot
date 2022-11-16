732ddacc4ec6e27ba03f8fc58d6febf79a16be9e
Franck Salzard <franck.salzard@etu.univ-grenoble-alpes.f>
2022-11-16 19:36:27 +0100

    ok test

--- jpa/src/main/java/com/example/jpa/ProductController.java
+++ jpa/src/main/java/com/example/jpa/ProductController.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +19,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@CrossOrigin(origins = "http://localhost:8081") // parametre des ports cohérents (on peut peut etre faire autrement )
+//@CrossOrigin(origins = "http://localhost:8081") // parametre des ports cohérents (on peut peut etre faire autrement )
+//actuellement 8080
 @RestController
 //@RequestMapping("/api") // faire des chemins corrects
 public class ProductController {
@@ -65,7 +66,7 @@ public class ProductController {
 		}
 	}
 
-	@PutMapping("/tutorials/{id}")
+	@PutMapping("/products/{id}")
 	public ResponseEntity<Product> updateProduct(@PathVariable("id") long id, @RequestBody Product product) {
 		Optional<Product> productData = productRepository.findById(id);
 
---
+++ jpa/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO product (id, name,price,quantity) VALUES (1, 'Apple',1000,10);
+INSERT INTO product (id, name,price,quantity) VALUES (2, 'Samsung',900,20);
+INSERT INTO product (id, name,price,quantity) VALUES (3, 'Google',800,30);
+INSERT INTO product (id, name,price,quantity) VALUES (4, 'Xiaomi',700,40);
+INSERT INTO product (id, name,price,quantity) VALUES (5, 'Sony',600,50);
