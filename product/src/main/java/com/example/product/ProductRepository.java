b68ff8f561e297f32721a0253142ba4633580222
Franck Salzard <franck.salzard@etu.univ-grenoble-alpes.f>
2022-11-16 19:08:49 +0100

    ok

--- product/src/main/java/com/example/product/ProductRepository.java
+++ product/src/main/java/com/example/product/ProductRepository.java
@@ -1,11 +1,11 @@
 package com.example.product;
 
-import java.util.List;
+//import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-  List<Product> findByName(String Name);
+  //List<Product> findByName(String Name);
 
 } 
