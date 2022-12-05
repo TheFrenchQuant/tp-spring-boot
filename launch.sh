594f60b6644b53ecb9759c702f7e55b8e72bbcb9
salzardf <franck.salzard@etu.univ-grenoble-alpes.fr>
2022-12-05 11:08:50 +0100

    ok

--- launch.sh
+++ launch.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 #En fait pas vraiment nécessaire
 
-gnome-terminal --"java -jar eureka/target/eureka-0.0.1-SNAPSHOT.jar"
+taskset -c 0 java -jar eureka/target/eureka-0.0.1-SNAPSHOT.jar
 
-gnome-terminal --"java -jar admin/target/admin-0.0.1-SNAPSHOT.jar"
+taskset -c 1 java -jar admin/target/admin-0.0.1-SNAPSHOT.jar
 
-gnome-terminal --"java -jar product/target/product-0.0.1-SNAPSHOT.jar"
+taskset -c 2 java -jar product/product-service/target/product-service-0.0.1-SNAPSHOT.jar
 
-gnome-terminal --"java -jar invoice/target/invoice-0.0.1-SNAPSHOT.jar --server.port=8081"
+taskset -c 3 java -jar invoice/invoice-service/target/invoice-service-0.0.1-SNAPSHOT.jar --server.port=8081
 
-gnome-terminal --"java -jar invoice/target/invoice-0.0.1-SNAPSHOT.jar --server.port=8082"
+taskset -c 4 java -jar invoice/invoice-service/target/invoice-service-0.0.1-SNAPSHOT.jar --server.port=8082
 
-gnome-terminal --"java -jar invoice/target/invoice-0.0.1-SNAPSHOT.jar --server.port=8082"
+taskset -c 5 java -jar invoice/invoice-service/target/invoice-service-0.0.1-SNAPSHOT.jar --server.port=8083
 
-gnome-terminal --"java -jar loadbalancer/target/loadbalancer-0.0.1-SNAPSHOT.jar"
+taskset -c 6 java -jar loadbalancer/target/loadbalancer-0.0.1-SNAPSHOT.jar
+
