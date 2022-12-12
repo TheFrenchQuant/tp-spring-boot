f02fe1f12cbc82dadd7faa2fb1a2ed094aa96459
TheFrenchQuant <franck.salzard@gmail.com>
2022-12-12 22:19:16 +0100

    ok

--- add-instance.sh
+++ add-instance.sh
@@ -2,6 +2,6 @@
 
 gnome-terminal -e "taskset -c 6 java -jar invoice/invoice-service/target/invoice-service-0.0.1-SNAPSHOT.jar --server.port=8082"
 
-#gnome-terminal -e "taskset -c 7 java -jar invoice/invoice-service/target/invoice-service-0.0.1-SNAPSHOT.jar --server.port=8083"
+gnome-terminal -e "taskset -c 7 java -jar invoice/invoice-service/target/invoice-service-0.0.1-SNAPSHOT.jar --server.port=8083"
 
 
